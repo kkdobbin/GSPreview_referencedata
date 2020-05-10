a081fb9231ec08e02c34f37a4ec059e6efe348fc
Kristin Dobbin <kbdobbin@ucdavis.edu>
2020-05-10 15:46:30 -0700

    Updated ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # GSPreview_referencedata
 Data and scripts for reference data used in GSP review project April/May 2020
+
+#What is where
+Data folder has faw CSV outputs from spatial joins
+Scripts folder has R script used to compile all but one of the data files in the data folder into a master summary data table
+Finaldata has the output of the data compilation script and a reduced version of the raw domestic well summary data which could not easily be joined with the other data due to slightly differing GSA names. This final join was done by hand in preparing individual data tables for GSPs
--- Scripts/Datacompilation.R
+++ Scripts/Datacompilation.R
@@ -11,45 +11,49 @@ domwells <- read.csv("Data/Domesticwells.csv")
 
 # Making summary table components
 #Cities
-Test1 <- Cities %>%
+Column1 <- Cities %>%
   group_by(GSA_Name) %>%
   summarize("City names" = paste(unique(Place), collapse = ", "))
 #DACs
-Test2 <- DACs %>%
+Column2 <- DACs %>%
   group_by(GSA_Name) %>%
   summarize("DAC names" = paste(unique(DAC_NAMELSAD), collapse = ", "))
 #CWSs
-Test3 <- CWSs %>%
+Column3 <- CWSs %>%
   group_by(GSA_Name) %>%
   summarize("CWS names" = paste(unique(PWS_name), collapse = ", "))
 #PWSs
-Test4 <- PWSs %>%
+Column4 <- PWSs %>%
   group_by(GSA_Name) %>%
   summarize("PWS names" = paste(unique(PWS_name), collapse = ", "))
 #number cities
-Test5 <- Cities %>% group_by(GSA_Name) %>% summarize("Number of cities" = n_distinct(Place))
+Column5 <- Cities %>% group_by(GSA_Name) %>% summarize("Number of cities" = n_distinct(Place))
 #Number DACs
-Test6 <- DACs %>% group_by(GSA_Name) %>% summarize("Number of DAC places" = n_distinct(DAC_NAMELSAD))
+Column6 <- DACs %>% group_by(GSA_Name) %>% summarize("Number of DAC places" = n_distinct(DAC_NAMELSAD))
 #Number CWSs
-Test7 <- CWSs %>% group_by(GSA_Name) %>% summarize("Number of Community Water Systems (CWSs)" = n_distinct(PWS_name))
+Column7 <- CWSs %>% group_by(GSA_Name) %>% summarize("Number of Community Water Systems (CWSs)" = n_distinct(PWS_name))
 #Number PWSs
-Test8 <- PWSs %>% group_by(GSA_Name) %>% summarize("Number of Public Water Systems (PWSs)" = n_distinct(PWS_name))
+Column8 <- PWSs %>% group_by(GSA_Name) %>% summarize("Number of Public Water Systems (PWSs)" = n_distinct(PWS_name))
 #Number of public supply wells
-Test9 <- Pubwells %>% group_by(GSA_Name) %>% summarize("Number of Public Supply Wells" = n())
+Column9 <- Pubwells %>% group_by(GSA_Name) %>% summarize("Number of Public Supply Wells" = n())
 #Number of domestic supply wells already summarized
 domwells <- domwells %>% rename('Number of Domestic Wells' = Domestic.Wells.Per.GSA) 
 
 #Join
-Data1 <- full_join(Test1, Test2, by = "GSA_Name")
-Data2 <- full_join(Data1, Test3, by = "GSA_Name")
-Data3 <- full_join(Data2, Test4, by = "GSA_Name")
-Data4 <- full_join(Data3, Test5, by = "GSA_Name")
-Data5 <- full_join(Data4, Test6, by = "GSA_Name")
-Data6 <- full_join(Data5, Test7, by = "GSA_Name")
-Data7 <- full_join(Data6, Test8, by = "GSA_Name")
-Data8 <- full_join(Data7, Test9, by = "GSA_Name")
+Data1 <- full_join(Column1, Column2, by = "GSA_Name")
+Data2 <- full_join(Data1, Column3, by = "GSA_Name")
+Data3 <- full_join(Data2, Column4, by = "GSA_Name")
+Data4 <- full_join(Data3, Column5, by = "GSA_Name")
+Data5 <- full_join(Data4, Column6, by = "GSA_Name")
+Data6 <- full_join(Data5, Column7, by = "GSA_Name")
+Data7 <- full_join(Data6, Column8, by = "GSA_Name")
+Data8 <- full_join(Data7, Column9, by = "GSA_Name")
 Finaldata <- Data8
 Finaldata <- Finaldata[c(1,6,2,7,3,8,4,9,5,10)]
 
 #export csv
-write.csv(Finaldata, file = "Outputs/Finaldata.csv", row.names = F)
+write.csv(Finaldata, file = "FinalData/Joineddata.csv", row.names = F)
+
+#Write out a data set for domestic wells 
+DomesticWellData <- domwells[-3]
+write.csv(DomesticWellData, file = "FinalData/domesticwelldata.csv", row.names = F)
